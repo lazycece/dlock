8a4c7a26987b0fd0b48c08c9747f3a7dd3f8fd1f
lazycece <lazycece@gmail.com>
2025-05-01 01:10:08 +0800

    init dlock sample

---
+++ dlock-samples/src/main/java/com/lazycece/dlock/samples/DlockSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.dlock.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlockSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlockSamplesApplication.class, args);
+    }
+
+}
---
+++ dlock-samples/src/test/java/com/lazycece/dlock/samples/DlockSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazycece.dlock.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlockSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
