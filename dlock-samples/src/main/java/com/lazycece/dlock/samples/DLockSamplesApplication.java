b918959a0c18e32b0511a7200e46469cfcba82e1
lazycece <lazycece@gmail.com>
2025-05-01 01:11:46 +0800

    mdf

--- dlock-samples/src/main/java/com/lazycece/dlock/samples/DLockSamplesApplication.java
+++ dlock-samples/src/main/java/com/lazycece/dlock/samples/DLockSamplesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DlockSamplesApplication {
+public class DLockSamplesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DlockSamplesApplication.class, args);
+        SpringApplication.run(DLockSamplesApplication.class, args);
     }
 
 }
