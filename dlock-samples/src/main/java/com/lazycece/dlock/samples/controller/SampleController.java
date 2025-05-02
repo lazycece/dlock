d177dfa6db13ffcbc974bb15d86e661aa944347d
lazycece <lazycece@gmail.com>
2025-05-02 22:00:33 +0800

    sample and ut

--- dlock-samples/src/main/java/com/lazycece/dlock/samples/controller/SampleController.java
+++ dlock-samples/src/main/java/com/lazycece/dlock/samples/controller/SampleController.java
@@ -16,7 +16,10 @@
 
 package com.lazycece.dlock.samples.controller;
 
+import com.lazycece.dlock.core.DLock;
+import com.lazycece.dlock.core.DLockFactory;
 import com.lazycece.dlock.core.DLockUtils;
+import com.lazycece.dlock.core.exception.DLockTimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,18 +36,81 @@ public class SampleController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/sample")
-    public void springSample() throws InterruptedException {
-        log.info("================== spring sample begin");
+    public String sample() {
+        log.info("================== sample begin");
 
-        DLockUtils.tryLock("springSample", 5, TimeUnit.MINUTES, () -> {
-            log.info("================== spring sample handle something .");
-            try {
-                Thread.sleep(10 * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        log.info("================== spring sample end");
+        try {
+            DLockUtils.tryLock("sample", 60, TimeUnit.SECONDS, () -> {
+                log.info("================== sample handle something");
+                try {
+                    Thread.sleep(15 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (DLockTimeoutException e) {
+            return e.getMessage();
+        }
+        log.info("================== sample end");
+        return "sample end";
     }
+
+    @GetMapping("/renewals")
+    public String renewals() {
+        log.info("================== renewals begin");
+
+        try {
+            DLockUtils.tryLock("renewals", 30, TimeUnit.SECONDS, () -> {
+                log.info("================== renewals handle something");
+                try {
+                    Thread.sleep(29 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (DLockTimeoutException e) {
+            return e.getMessage();
+        }
+        log.info("================== renewals end");
+        return "renewals end";
+    }
+
+    @GetMapping("/reentrant")
+    public String reentrant() throws InterruptedException {
+        log.info("================== reentrant begin");
+
+        DLock lock = DLockFactory.getInstance().produce("reentrant");
+        if (lock.tryLock(60, TimeUnit.SECONDS)) {
+            try {
+                if (lock.tryLock(60, TimeUnit.SECONDS)) {
+                    try {
+                        Thread.sleep(20 * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            lock.unlock();
+                        } catch (Exception e) {
+                            log.error("unlock error, lockKey = {}", "reentrant", e);
+                        }
+                    }
+                } else {
+                    throw new DLockTimeoutException("try lock timeout!");
+                }
+            } finally {
+                try {
+                    lock.unlock();
+                } catch (Exception e) {
+                    log.error("unlock error, lockKey = {}", "reentrant", e);
+                }
+            }
+        } else {
+            throw new DLockTimeoutException("try lock timeout!");
+        }
+
+        log.info("================== reentrant end");
+        return "reentrant end";
+    }
+
+
 }
